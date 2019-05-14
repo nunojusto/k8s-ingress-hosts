09ee7e5be6d3daaec8cc44e85ad3192f11a0f26f
Staffan Olsson <staffan@repos.se>
2019-05-14 14:51:45 +0200

    Clean up a bit better after release

--- release.sh
+++ release.sh
@@ -5,6 +5,6 @@ TAG=$1
 
 ./build.sh
 curl -Lo hub.tgz https://github.com/github/hub/releases/download/v2.11.2/hub-darwin-amd64-2.11.2.tgz
-tar xvzf hub.tgz
+tar xzf hub.tgz && rm hub.tgz
 ./hub-darwin-amd64-2.11.2/bin/hub release create -a release/k8s-ingress-hosts-darwin-amd64 -a release/k8s-ingress-hosts-darwin-amd64.sha256 -a release/k8s-ingress-hosts-linux-amd64 -a release/k8s-ingress-hosts-linux-amd64.sha256 $TAG
 rm -r hub-darwin-amd64-2.11.2*
